c708edf31ac29e0deac3038be4507b8719656e3a
Kirthi JC <its.me.jckirthi@gmail.com>
2025-08-24 00:44:29 +0530

    Update 0014-longest-common-prefix.java

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -2,11 +2,14 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
 
-        String prefix = strs[0];
+        String prefix = strs[0]; // 1st one is checked in others
 
-        for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
+        for (int i = 1; i < strs.length; i++) 
+        {
+            while (strs[i].indexOf(prefix) != 0) //checks if prefix==that string (==0 when first position matches) 
+            {
+                //If the current string does not start with prefix keep shortening prefix by removing the last character.
+                prefix = prefix.substring(0, prefix.length() - 1); 
                 if (prefix.isEmpty())
                  return "";
             }

c7a747b6728f2c120b1d93da1b0fe24a4de083c6
Kirthi JC <its.me.jckirthi@gmail.com>
2025-08-24 01:21:05 +0530

    Update 701203-equilibrium-point.java

--- 701203-equilibrium-point-GFG/701203-equilibrium-point.java
+++ 701203-equilibrium-point-GFG/701203-equilibrium-point.java
@@ -1,21 +1,25 @@
-class Solution {
-    // Function to find equilibrium point in the array.
-    public static int findEquilibrium(int arr[]) {
+class Solution 
+{
+        public static int findEquilibrium(int arr[])
+    {
         int n = arr.length;
         long total = 0;
         
         for (int i = 0; i < n; i++) {
-            total += arr[i];
+            total += arr[i]; //sum of all the elements in the array
         }
-        
+
+        // sum of all elements - sum of first i elements == remaining of the total -----> equilibrium
         long leftSum = 0;
-        for (int i = 0; i < n; i++) {
-            total -= arr[i]; 
-            if (leftSum == total) {
+        for (int i = 0; i < n; i++)
+            {
+            total -= arr[i]; //removing elements
+            if (leftSum == total) //checking if 1st half == 2nd half(as total is reduced)
+            {
                 return i;  // equilibrium index found
             }
             
-            leftSum += arr[i];
+            leftSum += arr[i]; //increasing the 1st half
         }
         
         return -1; // no equilibrium

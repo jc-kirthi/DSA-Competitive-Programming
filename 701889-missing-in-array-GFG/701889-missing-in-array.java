7ceff5b10c97f1f1ed0418ed0eab197488cedcad
Kirthi JC <its.me.jckirthi@gmail.com>
2025-08-24 00:59:32 +0530

    Update 701889-missing-in-array.java

--- 701889-missing-in-array-GFG/701889-missing-in-array.java
+++ 701889-missing-in-array-GFG/701889-missing-in-array.java
@@ -1,11 +1,13 @@
 class Solution {
     int missingNum(int arr[]) {
-        int n = arr.length + 1;   
+        int n = arr.length + 1;  // +1 beacuse 21 element is missing
+        
+        //sum of first n natural numbers formula(as we start from 1 to n)
         long total = (long)n * (n + 1) / 2;  // use long to avoid overflow
 
         long sum = 0;
         for (int num : arr) {
-            sum += num;
+            sum += num; // sum of existing elements
         }
 
         return (int)(total - sum);  // cast back to int at the end

a2372d90f3ef19e3eb62a323a865d047bc43c795
Kirthi JC <its.me.jckirthi@gmail.com>
2025-08-24 14:01:00 +0530

    Time: 0 ms (100%), Space: 41 MB (100%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -2,13 +2,16 @@ class Solution
 {
     public int gcdOfOddEvenSums(int n) 
     {
-        /** 
+        /**
+        method -------1 
         int sumOdd=0;
         int sumEven=0;
         int gcd=1;
 
+//method -----3
          int sumOdd = n * n;
         int sumEven = n * (n + 1); 
+
 
         for (int i = 1; i <= 2*n; i += 2) {
             sumOdd += i;
@@ -22,10 +25,32 @@ class Solution
                 gcd=k;
             }
             return gcd; */
+
+
+/** 
+            // method-------2
              int sumOdd = n^2;
-    int sumEven = n*(n+1);
-    
-    return n;
+            int sumEven = n*(n+1);
+            //GCD of n^2 and n*(n+1) = n because n and n+1 are coprime
+            //GCD(n^2, n*(n+1)) = n * GCD(n, n+1)
+           //n and n+1 are consecutive numbers, so GCD(n, n+1) = 1
+           // GCD(sumOdd,sumEven)=n∗1=n
+             return n;
+*/
+            int a = n * n; //odd terms
+        int b = n * (n + 1); //even terms 
+
+        while(a>0&& b>0)
+        {
+          if(a>b)
+          a=a%b;
+          else
+          b=b%a; 
+          if(a==0)
+          return b;
+        }
+        
+return a;
     }
 }
    
